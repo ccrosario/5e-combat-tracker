e1e4a43af3b3fc286cf5386b6c2f84a5d3f1d8bf
Cesar Rosario <cesar.c.rosario@gmail.com>
2024-01-23 14:48:16 -0500

    adding notes

--- 5e-combat-tracker.py
+++ 5e-combat-tracker.py
@@ -2,14 +2,17 @@ import pandas
 import pyfiglet
 import random
 
+#declare global values
 registeringCombatant = {}
 initiativeOrder = []
 initiativeRoll = int
 registration = True
 
+#styled name heading
 trackerHeading1 = pyfiglet.figlet_format('   Combat', font="cyberlarge")
 trackerHeading2 = pyfiglet.figlet_format('  Tracker', font="cyberlarge")
 
+#add combatant to order
 def addToOrder():
     userInput = input('Enter a combatant: ')
     registeringCombatant[0, 0] = userInput
@@ -31,6 +34,7 @@ def addToOrder():
     printInitiative()
 
 
+#[WIP]remove combatant from order
 def removeFromOrder():
     result = None
     combatantToRemove = input('Enter the name of the combatant you would like to remove: ')
@@ -41,6 +45,7 @@ def removeFromOrder():
             print(result)
 
 
+#print the current iniative order to the console
 def printInitiative():
     initiativeTable = pandas.DataFrame(initiativeOrder,
         columns=['Name', 'Hit Points', 'DEX Modifier', 'Bonuses', 'Initiative'])
@@ -48,6 +53,7 @@ def printInitiative():
     print('\n')
 
 
+#loading image/intro screen/ascii art
 print('                                      /|\n                                     |\\|\n                                     |||\n                                     |||\n                                     |||\n                                     |||\n                                     |||\n                                     |||\n                                  ~-[{o}]-~\n                                     |/|\n                                     |/|\n             ///~`     |\\\\_          `0\'         =\\\\\\\\         . .\n            ,  |=\'  ,))\\_| ~-_                    _)  \\      _/_/|\n           / ,\' ,;((((((    ~ \\                  `~~~\\-~-_ /~ (_/\\\n         /\' -~/~)))))))\'\\_   _/\'                      \\_  /\'  D   |\n        (       (((((( ~-/ ~-/                          ~-;  /    \\--_\n         ~~--|   ))\'\'    \')  `                            `~~\\_    \\   )\n             :        (_  ~\\           ,                    /~~-     ./\n              \\        \\_   )--__  /(_/)                   |    )    )|\n    ___       |_     \\__/~-__    ~~   ,\'      /,_;,   __--(   _/      |\n  //~~\\`\\    /\' ~~~----|     ~~~~~~~~\'        \\-  ((~~    __-~        |\n((()   `\\`\\_(_     _-~~-\\                      ``~~ ~~~~~~   \\_      /\n )))     ~----\'   /      \\                                   )       )\n  (         ;`~--\'        :                                _-    ,;;(\n            |    `\\       |                             _-~    ,;;;;)\n            |    /\'`\\     ;                          _-~          _/\n           /~   /    |    )                         /;;;\'\'  ,;;:-~\n          |    /     / | /                         |;;\'   ,\'\'\n          /   /     |  \\\\|                         |   ,;(    -Tua Xiong\n        _/  /\'       \\  \\_)                   .---__\\_    \\,--._______\n       ( )|\'         (~-_|                   (;;\'  ;;;~~~/\' `;;|  `;;;\\\n        ) `\\_         |-_;;--__               ~~~----__/\'    /\'_______/\n        `----\'       (   `~--_ ~~~;;------------~~~~~ ;;;\'_/\'\n                     `~~~~~~~~\'~~~-----....___;;;____---~~\n')
 print(trackerHeading1, trackerHeading2)
 print('                 You have found yourself in combat once again...\n')
@@ -55,12 +61,14 @@ print('    ╰ ─┉─¡! • !¡─┉─ ╯╰ ─┉─¡! • !¡─┉�
 print('                     ╰ ─┉─¡! • !¡─┉─ ╯╰ ─┉─¡! • !¡─┉─ ╯\n')
 
 
+#initiate registration on load
 while registration:
     addToOrder()
     moreCombatants = input('Are there any other combatants? (y/n): ')
     if moreCombatants == 'n':
         registration = False
 
+#main menu
 while not registration:
     print('And combat begins...\n---------------------')
     printInitiative()
